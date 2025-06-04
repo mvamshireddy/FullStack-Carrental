5010227ca35fc2c5bef5d39278558ccf92d37b6e
mvamshireddy <98573876+mvamshireddy@users.noreply.github.com>
2025-06-04 13:15:32 +0530

    .

---
+++ backend/testmail.js
@@ -0,0 +1,6 @@
+const { sendWelcomeMail } = require('./utils/mailer');
+sendWelcomeMail('mvamshireddy9848@gmail.com', 'Test User').then(() => {
+  console.log('Mail sent!');
+}).catch(e => {
+  console.error('Mail failed:', e);
+});
